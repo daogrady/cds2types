b4fa91bc190cc892bb82861174ca529e7db4fbab
Daniel O'Grady <daniel.o-grady@sap.com>
2022-04-27 08:51:43 +0200

    Fix test suite for array util

--- src/array.util.ts
+++ src/array.util.ts
@@ -17,16 +17,19 @@ export const replace = (
 ): string => string.replace(pattern, replacement);
 
 export const startCase = (str: string): string =>
-    str
-        .split(/(?=[A-Z\s])/)
+    (
+        str.replace(/[-_]+/g, " ").match(/[A-Z]+(?![a-z])|[A-Z]?[a-z]+|\d+/g) ??
+        []
+    )
         .map((s) => s.trim())
-        .filter((s) => !!s)
+        .filter((s) => !!s) // empty strings
         .map((s) => s.charAt(0).toUpperCase() + s.slice(1))
         .join(" ");
 
 export const join = <T>(xs: Array<T>, glue: string) => xs.join(glue);
 
-export const takeRight = <T>(xs: Array<T>, n: number): Array<T> => xs.slice(-n);
+export const takeRight = <T>(xs: Array<T>, n: number): Array<T> =>
+    n > 0 ? xs.slice(-n) : [];
 
 export const flatten = <T>(xss: Array<Array<T>>): Array<T> =>
     xss.reduce((prev, current) => prev.concat(current), []);
--- test/jest/array.util.test.ts
+++ test/jest/array.util.test.ts
@@ -20,15 +20,15 @@ const randInts = (n: number, lo: number, hi: number) =>
     genArray(n, () => randInt(lo, hi));
 
 // output of _ === output of au
-//const equal = (s, apply, args) => test(s, apply(_, args) => expect(f(_)).toBe(f(au)));
 const equal = (
     s: string,
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     apply: (lib: any, ...args: any) => unknown,
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     args: any
 ) =>
     test(`${s}(${args})`, () =>
-        expect(apply(_, args)).toStrictEqual(apply(au, args)));
+        expect(apply(au, args)).toStrictEqual(apply(_, args)));
 
 equal("min", (lib, args) => lib.min(args), randInts(10, -100, 100));
 equal("last", (lib, args) => lib.last(args), randInts(10, -100, 100));
@@ -50,17 +50,31 @@ equal(
     (lib, [str, what, repl]) => lib.replace(str, what, repl),
     [randStr(100), /\s/, "b"]
 );
-equal("startcase", (lib, args) => lib.startCase(args), randStr(1000));
 equal("join", (lib, [xs, glue]) => lib.join(xs, glue), [
     randStr(1000).split(" "),
     "+",
 ]);
-
 equal("takeRight", (lib, [xs, n]) => lib.takeRight(xs, n), [
     randInts(10, -10, 10),
     randInt(-2, 21),
 ]);
+equal("takeRight", (lib, [xs, n]) => lib.takeRight(xs, n), [[1, 2, 3, 4], -2]);
 equal("flatten", (lib, args) => lib.flatten(args), [
     randInts(10, -10, 10),
     randInts(10, -10, 10),
 ]);
+equal("startcase", (lib, args) => lib.startCase(args), randStr(1000));
+equal("startcase", (lib, args) => lib.startCase(args), "--foo-bar--");
+equal("startcase", (lib, args) => lib.startCase(args), "fooBar");
+equal("startcase", (lib, args) => lib.startCase(args), "__FOO_BAR__");
+
+/*
+_.startCase('--foo-bar--');
+// => 'Foo Bar'
+ 
+_.startCase('fooBar');
+// => 'Foo Bar'
+ 
+_.startCase('__FOO_BAR__');
+// => 'FOO BAR'
+*/
